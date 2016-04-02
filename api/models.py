7574bc105960e16a3b9d06a642f3de9e13d3a907
smenon18 <smenon18@gmail.com>
2016-04-01 22:57:22 -0400

    created api models, linked urls for api and restframework

--- api/models.py
+++ api/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True, null=False, blank=False)
+    password = models.CharField(max_length=32, null=False, blank=False)
+    acctype = models.BooleanField(default=False) # False is child, True is parent
+
+class ParentalRel(models.Model):
+    childUser = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    parentUser = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
